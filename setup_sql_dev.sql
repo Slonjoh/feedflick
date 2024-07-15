6857f6f9a96361de6672764d86527bedc68de60b
Slonjoh <tobson11122@gmail.com>
2024-07-15 19:00:48 +0000

    mysql setup

---
+++ setup_sql_dev.sql
@@ -0,0 +1,11 @@
+-- Create or use the feedflick_dev_db database
+CREATE DATABASE IF NOT EXISTS feedflick_dev_db;
+
+-- Create or use the feedflick_dev user with the password 'feedflick_dev_pwd' on localhost
+CREATE USER IF NOT EXISTS 'feedflick_dev'@'localhost' IDENTIFIED BY 'feedflick_dev_pwd';
+
+-- Grant all privileges on the feedflick_dev_db database to the feedflick_dev user
+GRANT ALL PRIVILEGES ON feedflick_dev_db.* TO 'feedflick_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'feedflick_dev'@'localhost';
---
+++ setup_sql_test.sql
@@ -0,0 +1,11 @@
+-- Create or use the feedflick_test_db database
+CREATE DATABASE IF NOT EXISTS feedflick_test_db;
+
+-- Create or use the feedflick_test user with the password 'feedflick_test_pwd' on localhost
+CREATE USER IF NOT EXISTS 'feedflick_test'@'localhost' IDENTIFIED BY 'feedflick_test_pwd';
+
+-- Grant all privileges on the feedflick_test_db database to the feedflick_test user
+GRANT ALL PRIVILEGES ON feedflick_test_db.* TO 'feedflick_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to the hbnb_test user
+GRANT SELECT ON performance_schema.* TO 'feedflick_test'@'localhost';
