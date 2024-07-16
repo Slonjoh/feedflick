38761833f4041abbff14f9ad87ebbb6e7932fedb
Slonjoh <tobson11122@gmail.com>
2024-07-16 02:28:10 +0000

    starting frontend

---
+++ frontend/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container7"></div>
+    </footer>
+  );
+}
+
+export default Footer;
+
