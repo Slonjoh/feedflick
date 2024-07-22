4166cf71410047a66b57cc44c3931ae99f2f6d31
Slonjoh <tobson11122@gmail.com>
2024-07-22 03:26:48 +0000

    login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-
+import './Login.css';
 const Login = () => {
   const [formData, setFormData] = useState({
-    username: '',
+    email: '',
     password: ''
   });
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -12,38 +14,74 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(formData);
     // Implement your login logic here
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="login-form">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
+    <div>
+      <header>
+        <div className="header-content">
+          <div className="header-logo">
+            <img src={`${process.env.PUBLIC_URL}/images/feedflick-logo.svg`} alt="Feedflick Logo" />
+            <span>feedflick</span>
+          </div>
+          <nav className={menuOpen ? 'active' : ''}>
+            <ul>
+              <li><a href="/publish">Publish</a></li>
+              <li><a href="/engage">Engage</a></li>
+              <li><a href="/socials">Socials</a></li>
+              <li><a href="/dashboard">Dashboard</a></li>
+            </ul>
+          </nav>
+          <div className="menu-icon" onClick={toggleMenu}>
+            &#9776;
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
+	<h5 className="text">Don’t have an account?</h5>
+        <div className="auth-buttons">
+          <button>
+            <a href="/signup">Get Started <span className="arrow">→</span></a>
+          </button>
         </div>
-        <button type="submit">Login</button>
-      </form>
+      </header>
+      <div className="login-container">
+        <form onSubmit={handleSubmit} className="login-form-grid">
+          <h4 className="login-form-title">Login Here </h4>
+          <div className="login-form">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+	      placeholder="Enter your email"
+              value={formData.username}
+              onChange={handleChange}
+	      required
+            />
+          </div>
+          <div className="login-form">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+	      placeholder="Enter your password"
+              value={formData.password}
+              onChange={handleChange}
+	      required
+            />
+          </div>
+          <div className="button-group">
+            <button type="submit">Login </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-};
+}
 
 export default Login;
 
